6ee5cb48afce8e90aad767535c7802394cb57734
NefertariLaurel <laurel_04@outlook.com>
2023-08-27 11:09:08 -0600

    Add code jwy and bcrypt

---
+++ src/lib/bcrypt.js
@@ -0,0 +1,18 @@
+/* iniciar */ 
+
+const bcryptjs = require("bcryptjs");
+
+const saltRounds = 10;
+
+function encrypt(text) {
+  return bcryptjs.hashSync(text, saltRounds);
+}
+
+function verify(hash, text) {
+  return bcryptjs.compareSync(text, hash);
+}
+
+module.exports = {
+  encrypt,
+  verify,
+};
