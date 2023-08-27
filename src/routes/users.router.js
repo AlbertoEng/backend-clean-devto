c12a1a3322d6e31e6c0d5565edad93672a737cdc
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2023-08-27 11:37:36 -0600

    agregando users router

---
+++ src/routes/users.router.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const users = require("../usecases/users.usecase")
+const auth = require("../middlewares/auth.middleware")
+const router = express.Router()
+
+// Obtener informacion de un usuario por id 
+router.get('/:id', async (request, response) => {
+    try {
+        const id = request.params.id
+        const user = await users.getById(id)
+    
+        response.json({
+            message: `User: ${user.firstName} id: ${id}`,
+            data: { user }
+        })
+    } catch (error) {
+            response.status(error.status || 500)
+            response.json({
+                message:"Something went wrong",
+                error: error.message
+            })
+        }
+    })
+// Registrar un nuevo usuario
+router.post('/', async (request, response) => {
+    try {
+        const userData = request.body
+        const newUser = await users.create(userData)
+    
+        response.status(201)
+        response.json({
+            message: "User created",
+            data: {
+                user: newUser
+            }
+        })
+    } catch (error) {
+        const status = error.name === `ValidaitionError` ? 400 : 500
+        response.status(status)
+        response.json({
+            message: "something went wrong",
+            error: error.message
+        })
+    }
+})
+
+module.exports = router
+
